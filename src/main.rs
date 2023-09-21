c76abd33d163ff67dccbbff34a127d38e48e7790
Alessandro <alessandro@desktop-ale.lan>
2023-09-21 21:20:26 +0200

    Move metadata_and_to_mp3 function to ffmpeg.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, fs};
-use std::path::PathBuf;
-use std::process::{Command, ExitCode};
+use std::env;
+use std::process::ExitCode;
+use which::which;
 
 mod config;
 use config::Config;
@@ -14,8 +14,7 @@ use spotify::api::{fetch_track, UrlType};
 
 mod youtube;
 
-use which::which;
-use crate::spotify::api::Track;
+mod ffmpeg;
 
 fn usage(program: &str) {
     eprintln!("Usage: {program} [SUBCOMMAND] [OPTIONS]");
@@ -97,7 +96,7 @@ fn entry() -> Result<(), ()> {
                             eprintln!("ERROR: failed to download video: {err}");
                         })?;
 
-                    metadata_and_to_mp3(&ffmpeg_path, &input_file, &track);
+                    ffmpeg::metadata_and_to_mp3(&ffmpeg_path, &input_file, &track);
                 }
                 UrlType::Playlist => {
                     unimplemented!("Playlist download")
@@ -115,58 +114,6 @@ fn entry() -> Result<(), ()> {
         }
     }
 }
-
-fn metadata_and_to_mp3(ffmpeg_path: &PathBuf, input_file: &String, t: &Track) {
-    let mut command = Command::new(&ffmpeg_path);
-
-    let artists = t
-        .artists
-        .iter()
-        .map(|artist| artist.name.clone())
-        .collect::<Vec<String>>()
-        .join("; ");
-    let album_artists = t
-        .album
-        .artists
-        .iter()
-        .map(|artist| artist.name.clone())
-        .collect::<Vec<String>>()
-        .join("; ");
-
-    command
-        .args(&[
-            "-i", &input_file,
-            "-i", &t.album.images[0].url,
-            "-metadata", &format!("title={}", t.name),
-            "-metadata", &format!("artist={}", artists),
-            "-metadata", &format!("album_artist={}", album_artists),
-            "-metadata", &format!("album={}", t.album.name),
-            "-metadata", &format!("date={}", t.album.release_date),
-            "-map", "0",
-            "-map", "1",
-            "-c:v", "mjpeg",
-            "-q:v", "2",
-            "-c:a", "libmp3lame",
-            "-q:a", "4",
-            "-id3v2_version", "3",
-            "-metadata:s:v", "title='Album cover'",
-            "-metadata:s:v", "comment='Cover (front)'",
-            &format!("{}.mp3", t.name),
-        ]);
-
-    let output = command.output().expect("Failed to execute ffmpeg");
-
-    if !output.status.success() {
-        eprintln!(
-            "ERROR: ffmpeg failed with exit code {}",
-            output.status.code().unwrap_or(1)
-        );
-        std::process::exit(1);
-    }
-
-    fs::remove_file(&input_file).expect("Failed to remove input file");
-}
-
 fn main() -> ExitCode {
     check_dependencies().err();
 
